63c34bbff986298460a57e2bea3aaf49ad3d69d6
Kiara <ksd11b@my.fsu.edu>
2018-09-27 05:34:00 -0400

    push

--- src/Test/Junit.java
+++ src/Test/Junit.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 
 public class Junit {
@@ -20,16 +21,73 @@ public class Junit {
 		
 	SREwebdriverScript SRE = new SREwebdriverScript();	
 @Given("^Web page is loaded$")
-@BeforeClass
+@BeforeClass  
 	public void openPage(){
 		SRE.open();
-		
+		assertTrue (SRE.waitForOfferPage()); 
+	//	SRE.closeBrowser();
 	}
-//Tests if the web page title is correct
+
+
+@When("^I have \\$(\\d+) dollars to offer$")
+public void moneyToOffer(String amount)  {
+SRE.enterOfferAmount(amount);
+}
 
 
 @Test
-@Then("^Check title$)
+@When("^a \\$(\\d+) down payment$")
+public void a_$_down_payment(String amount) {
+    SRE.entercashDownAmount(amount);  
+}
+
+@Test
+@When("^a \"([^\"]*)\" credit rating$")
+public void a_credit_score(String rating)  {
+    SRE.enterCreditRating(rating);
+    
+}
+
+@Test
+@Then("^I will be able to move to next steps$")
+public void i_will_be_able_to_move_to_next_steps()  {
+ SRE.nextSteps();  
+}
+
+@Test
+@Then("^enter my zip code of (\\d+)$")
+public void enter_my_zip_code(String zipcode)  {
+  SRE.enterZip(zipcode);
+}
+
+@Test
+@Then("^I will be able to continue and send progress$")
+public void i_will_be_able_to() {
+ SRE.sendProgress();  
+}
+/*
+@Test
+@Then("^enter my contact info$")
+public void enter_my_contact_info() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+@Test
+@Then("^confirm my pricing summary$")
+public void confirm_my_pricing_summary() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+@Test
+@Then("^send and view confirmation$")
+public void send_and_view_confirmation() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+
+@Test
+@Then("^Check title$")
 	public void titleChk() throws IOException{
 	String title=SRE.getTitle();
 	assertEquals(title,
@@ -74,5 +132,5 @@ public class Junit {
 	}
 
 	
-	
+	*/
 }
--- src/Test/SREwebdriverScript.java
+++ src/Test/SREwebdriverScript.java
@@ -4,35 +4,119 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 
 
 public class SREwebdriverScript {
-	WebDriver driver=new ChromeDriver();
+
+	public  WebDriver driver = new ChromeDriver();
+    public WebDriverWait wait = new WebDriverWait(driver, 750);
+
 	JavascriptExecutor js; 
+	public static By offerInput=By.xpath("//*[@id='finance-input-offer']");
+	public static By cashDownInput=By.id("finance-cash-down");
+	public static By creditRating=By.id("finance-credit-tier");
+	public static By nextStepsButtn=By.id("send-my-offer");
+	public static By zipInputbox=By.id("input-shopper-zip");
+	public static By continueBttn=By.id("dr_update_zip_continue");
+	public static By noThanksBttn=By.id("deal-summary-page-continue-link");
+	public static By sendPrgressBttn=By.xpath ("//*[@id='deal_summary_page_send_button']");
+	public static By dwnPymtTxtDisplay=By.id("dr-sp-payment-box-detail-down-payment");
+	public static By LexusDemoBanner=By.xpath("/html/body/div[1]/div[2]/div[1]/div/h1/a/span");
+	public static By offerFrame=By.xpath("//*[@id='mmd-frame-frame2']");
+	public static By termsFrame=By.xpath ("//*[@id='mmd-frame-shopperplatform']");
+	public static By financeTab=By.xpath("//*[@id='tab-finance']");
+	public static By dealSummary=By.id("sp_lead_form_title");
+	public String offerPage=("https://lexus.roimotors.com/new/LEXUS/2018-LEXUS-GS+350-9478ae060a0a00f93a13f68a2e7fb412.htm");  
+	//public static By offerFrame=By.xpath("/html/body/noscript/text()");
 	//WebElement table = driver.findElement(By.id("table")); 
-
-
+  
+    
 public void open() {
-driver.navigate().to("C:\\Users\\keesh\\Desktop\\apps\\index.html");
+
+    driver.get(offerPage);
+   // driver.close();
+   
+}
+
+public boolean waitForOfferPage() {
+	
+wait.until(ExpectedConditions.visibilityOfElementLocated(LexusDemoBanner));
+WebElement myPage = driver.findElement(LexusDemoBanner); 
+if (( myPage).isDisplayed()==true) {
+System.out.println("Page loaded");
+return true;}
+else {
+System.out.println("Page NOT loaded");
+return false;}
 
 }
 
+public void enterOfferAmount(String amount){
+	WebElement element = driver.findElement(offerFrame);
+	((JavascriptExecutor)driver).executeScript("arguments[0].removeAttribute('style')", element);
+	driver.switchTo().frame( driver.findElement( offerFrame ));
+
+System.out.println("Hidden attributes removed");
+
+driver.findElement(offerInput).click();
+driver.findElement(offerInput).clear();
+driver.findElement(offerInput).sendKeys(amount);
+}
+
+public void entercashDownAmount(String amount){
+driver.findElement(cashDownInput).click();
+driver.findElement(cashDownInput).clear();
+driver.findElement(cashDownInput).sendKeys(amount);
+
+}
+public void enterCreditRating(String tier){
+Select dropdown = new Select(driver.findElement(creditRating));
+dropdown.selectByVisibleText(tier);
+System.out.println("offer info complete");
+}
+
+public void nextSteps(){
+	driver.findElement(nextStepsButtn).click();
+System.out.println("Headed to next step");
+}
+
+public void enterZip(String zip){
+	driver.findElement(zipInputbox).click();
+	driver.findElement(zipInputbox).clear();
+	driver.findElement(zipInputbox).sendKeys(zip);
+	wait.until(ExpectedConditions.elementToBeClickable(continueBttn));
+System.out.println("Headed to Contact Info");
+}
+
+public void sendProgress(){
+	driver.findElement(continueBttn).click();
+	wait.until(ExpectedConditions.elementToBeClickable(termsFrame));
+	driver.switchTo().frame( driver.findElement(termsFrame));
+	wait.until(ExpectedConditions.elementToBeClickable(sendPrgressBttn));
+	driver.findElement(sendPrgressBttn).click();
+	wait.until(ExpectedConditions.visibilityOfElementLocated(dealSummary));
+System.out.println("On Contact Page");
+}
 
 public String getTitle() throws IOException {
 	String title=driver.getTitle().toString();
-	 driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+	driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 	System.out.println(title+" page fully loaded");
 	
 	return title;
